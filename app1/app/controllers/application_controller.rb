22d38b67a8294fb7673ee127e74107de54c4cfa7
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-10-29 12:48:34 +0100

    re-writing app/comments controller rules(+details)
    opening api post /sessions endpoint for not-authenticated request, to
    retrieve the user token via api
    
    including set_comment as before_action inside comments controller
    
    routing request from url /users/sign_in to users/sessions controllers

--- app1/app/controllers/application_controller.rb
+++ app1/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery 
-  acts_as_token_authentication_handler_for User
-  before_action :authenticate_user!
+  acts_as_token_authentication_handler_for User, except: [:create]
+  before_action :authenticate_user!, except: [:create]
 end
--- app1/app/controllers/comments_controller.rb
+++ app1/app/controllers/comments_controller.rb
@@ -1,12 +1,16 @@
 class CommentsController < ApplicationController
+  acts_as_token_authentication_handler_for User
+  before_action :authenticate_user!
+  before_action :find_comment, only: [:edit, :update, :show]
+  before_action :set_comment, only: [:new, :create]
+
   def index
     @comments = Comment.find_comments params[:description]
   end
 
-  def new; @comment = Comment.new; end
+  def new;end
 
   def create 
-    @comment = Comment.new comment_params
     @comment.user = current_user
     respond_to do |format|
       if @comment.save
@@ -21,12 +25,29 @@ class CommentsController < ApplicationController
     end
   end
 
-  def edit
-    comment = Comment.find params[:id]
-  end
+  def edit; end
   
+  def update 
+    @comment.user = current_user
+    respond_to do |format|
+      if @comment.update(comment_params)
+        flash[:notice] = 'Comment was successfully created'
+        format.html { redirect_to @comment }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        flash[:error] = 'Something went wrong'
+        format.html { render action: 'new' }
+        format.json { render json: {errors: @comment.errors}, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show; end
+
   private
+  def find_comment; @comment ||= Comment.find params[:id]; end
+  def set_comment; @comment ||= Comment.new comment_params; end
   def comment_params
-    params.require(:comment).permit(:message, :description)
+    params.require(:comment).permit(:message, :description) unless params[:comment].nil?
   end
 end

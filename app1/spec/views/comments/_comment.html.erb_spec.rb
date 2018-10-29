e0ef92dfd95172e3e17985d3bf46018eeb350502
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-10-29 19:15:34 +0100

    comments edit/show/new views(+details)
    creating a _form for comments
    
    writing edit/show/new views
    
    creating a partial for links

--- app1/spec/views/comments/_comment.html.erb_spec.rb
+++ app1/spec/views/comments/_comment.html.erb_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe "comments/_comment.html.erb", type: :view do
     it 'does not include the update link' do
       allow(view).to receive(:current_user) { FactoryBot.build_stubbed(:user) }
       render :partial => "comments/comment.html.erb", :locals => {:comment => comment }
-      expect(rendered).to_not match /update/
+      expect(rendered).to_not match /edit/
     end
 
     it 'includes the update link' do
       allow(view).to receive(:current_user) { comment.user }
       render :partial => "comments/comment.html.erb", :locals => {:comment => comment }
-      expect(rendered).to match /update/
+      expect(rendered).to match /edit/
     end
   end
 end

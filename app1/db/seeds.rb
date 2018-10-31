914ff02e8ec544e6f8f82b6544cd591ce7e12974
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-10-31 16:33:46 +0100

    updating links and seed data

--- app1/db/seeds.rb
+++ app1/db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times { FactoryBot.create(:comment)}
+User.create(email: 'tester@gmail.com', password: 'tester')
